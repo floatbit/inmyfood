0ef5ad31817788a7ab099c2017386ce882f13b1a
“Chris <chris@floatbit.com>
2024-09-18 13:50:39 -0400

    moved api key out of repo

--- functions.php
+++ functions.php
@@ -1,10 +1,9 @@
 <?php
 
-define('CHATGPT_API_KEY', 'sk-TUNvEqHvIB5kYW08l0WWMIssps1JvQzidq1RP-aAQaT3BlbkFJDAKUNuiR1l2qJPk7gT1G0rdW6QT1IVQvFk9kQca9gA');
-
 // uncomment this if the theme require some plugin to be installed
 // require_once 'includes/tgm.php';
 
+require_once 'includes/secrets.php';
 require_once 'includes/main.php';
 require_once 'includes/template-tags.php';
 require_once 'includes/blocks.php';
